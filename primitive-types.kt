d8fc6930eeffc782824096c88830f9bff700e428
isumaeru-ruzu <isumaeru7.ruzu@gmail.com>
2025-01-14 09:50:26 -0300

    Primitive Type in Kotlin

---
+++ primitive-types.kt
@@ -0,0 +1,24 @@
+fun main() {
+    //Tipos inteiros
+    val byte: Byte = 127 //Armazena números pequenos no espaço de -128 à 127
+    val short: Short = 27318 //Armazena números no espaço de -32768 à 32767
+    val int: Int = 27392741 //Armazena números médio no espaço de -2147483648 à 2147483647
+    val long: Long = 9222456723467 //Armazena números grandes no espaço de -9223372036854775808 à 9223372036854775807
+
+    //Tipos ponto flutuantes
+    val float: Float = 4.57f //Armazena números com 6 à 7 casas decimais (Lembre-se que para ser considerado tipo float, adicione um sufixo "f" no final do valor da variável)
+    val double: Double = 8.234251921 //Armazena números grandes com 15 à 16 casas decimais
+
+    //Tipos boleanos
+    val bool: Boolean = false //Armazena valores de veradeiro ou falso
+    val bool2: Boolean = true
+
+    //Tipo charactere
+    val char: Char = 'I' //Armazena um único caractere
+
+    //Tipo string
+    val string: String = "Isumaeru" //Armazena um conjunto de caractere (Exemplos: palavras, frases, textos...)
+
+    //Tipo array
+    val array: Array<Int> = arrayOf(1, 2, 3) //Armazena um conjunto de tipos a uma lista
+}
