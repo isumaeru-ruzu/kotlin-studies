806bb90651d6e8f3df557213d7e799e3b20332bf
isumaeru-ruzu <isumaeru7.ruzu@gmail.com>
2025-01-22 08:48:59 -0300

    Learning about flow control

---
+++ flow-control.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var canSearchRoute = false
+    var routeType = "Avião" //Tipos de rotas: Andando; Carro; Trem; Metro;
+
+    //if = Se
+    //else = Se não
+    //when = Quando (Parecido com o Switch de outras linguagens.)
+    
+    if (canSearchRoute == true){
+        when (routeType) {
+            "Andando" -> println("Traçando rota andando.")
+            "Carro" -> println("Traçando rota de carro.")
+            "Trem" -> println("Traçando rota de trem.")
+            "Metro" -> println("Traçando rota de metro.")
+            else -> println("Rota não implementada.")
+        }
+    } else{
+        println("Pesquisa por rotas não está disponível no momento.")
+    }
+}
